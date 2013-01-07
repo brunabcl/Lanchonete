128306f47e04468a842ac2dc451ef30815ae129b
brunabcl <brunabcl@hotmail.com>
2013-01-07 00:51:21 -0200

    tete

--- src/com/lanchonete/main/java/models/signup/Category.java
+++ src/com/lanchonete/main/java/models/signup/Category.java
@@ -1,29 +1,7 @@
 package models.signup;
 
-public class Category {
+public class Category extends Base{
 	
-	private int id;
-	private String name;
-	
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "Category [getId()=" + getId() + ", getName()=" + getName()
-				+ "]";
-	}
+
 
 }
