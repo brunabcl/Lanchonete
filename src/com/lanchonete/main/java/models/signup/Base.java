ccee27b229f47b1c0e3538fb74e632b570f123a8
parox <weslleywellington@gmail.com>
2013-01-07 00:29:43 -0200

    Add Base

---
+++ src/com/lanchonete/main/java/models/signup/Base.java
@@ -0,0 +1,28 @@
+package models.signup;
+
+public class Base {
+
+	private int id;
+	private String name;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Category [getId()=" + getId() + ", getName()=" + getName()
+				+ "]";
+	}
+}
